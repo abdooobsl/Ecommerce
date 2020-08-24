b3c91260d2855532e3b45bf9cdb14e27736553e9
abdooobsl <abdooobsl@gmail.com>
2020-08-24 14:18:38 +0200

    v

--- app/src/main/java/com/relabs/e_commerce/util/Common.java
+++ app/src/main/java/com/relabs/e_commerce/util/Common.java
@@ -7,6 +7,7 @@ import com.relabs.e_commerce.retrofit.RetrofitClint;
 public class Common {
 
     public static String BASE_URL = "http://192.168.1.7/storeprojectlaravel/public/api/";
+
     public static User CURRENT_USER = null;
     public static Boolean USER_PREFERENCE=false;
 
